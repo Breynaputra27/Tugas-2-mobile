72be3b98038826f2b74bc6588f3ca92d49b5fec8
Breynaputra27 <breynaputra1101@gmail.com>
2025-10-03 10:44:08 +0700

    update tugas 3

--- BreynaApps/app/src/main/java/com/example/breynaapps/MainActivity.kt
+++ BreynaApps/app/src/main/java/com/example/breynaapps/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.hellotoast
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.breynaapps.MainActivity2
+import com.example.breynaapps.R
+
+private val MainActivity.buttonBrowser: Any
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0  // variabel untuk angka
@@ -12,22 +19,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Hubungkan layout dengan kode
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage
+        )
 
-        // Tombol Count → angka bertambah
-        buttonCountUp.setOnClickListener {
-            mCount++
-            mShowCount.text = mCount.toString()
-        }
 
-        // Tombol Toast → munculkan popup angka
-        buttonToast.setOnClickListener {
-            val tulisan = mShowCount.text.toString()
-            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
-        }
+        buttonCountUp.setOnClickListener(View.OnClickListener {
+            mCount++;
+            Log.d("mCount", Integer.toString(mCount))
+            if (mShowCount != null)
+                mShowCount.text = mCount.toString()
+        })
+
+
+        buttonToast.setOnClickListener(View.OnClickListener {
+            val tulisan: String = mShowCount?.text.toString()
+            val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan "+tulisan, Toast.LENGTH_LONG)
+            toast.show()
+        })
+
+
+        buttonSwitchPage.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        })
+
+        buttonBrowser.setOnClickListener(View.OnClickListener {
+            // Implicit Intent: Hanya mendefinisikan aksi (ACTION_VIEW) dan data (URL)
+            val intentbrowse = Intent(Intent.ACTION_VIEW)
+            intentbrowse.setData(Uri.parse("https://www.google.com/"))
+            startActivity(intentbrowse)
+        })
     }
+    
+
 }
+
+private fun Any.setOnClickListener(onClickListener: View.OnClickListener) {}
